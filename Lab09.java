e37c9e59b2922f915c268936f03de413413423cd
shadowlaq <38447226+shadowlaq@users.noreply.github.com>
2020-10-26 21:10:29 -0700

    numba 9

---
+++ Lab09.java
@@ -0,0 +1,39 @@
+// Part a
+// PROGRAMMER: Aaron Escamilla
+// COURSE: CSC110(JAVA)
+// CLASS TIME: 10am
+// COMPLETION: 10/26/2020
+// ASSIGNMENT: Lab09 - COnversion Program for distances
+/* PURPOSE: This appllication
+    - Opens a file, duplicates the files and consoles the written content in process of writing to new file
+    - Demonstrate variable IPO layout (Input Section Processing Section, Output Section)
+*/
+/*INPUTS SECTION: Scanner utilized for user input
+    - named constants --> 
+        - String --> OUTPUT_FILE (name of output file)
+    - named user input variables --
+        - String --> file name for duplication, file name for output
+        - char   --> 
+        - double --> 
+        - int    --> 
+    - named calculated variables --
+        - String --> write to file user specified
+        - double --> 
+        - int    --> counter, file length
+        - Scanner--> fileInputName (finput)
+*/
+/*PROCESSING SECTION
+    - Part 1: while loop to continue accepting entries 
+    - Part 2: While loop to write data
+*/
+/*OUTPUTS
+    - System output paragraph with inserted answers
+    - Test cases: 
+      --> Part 1: fable.txt, blank.txt
+/*ERRORS
+   -
+*/
+/***********************************************************************************************/
+public class Lab09 {
+    
+}
